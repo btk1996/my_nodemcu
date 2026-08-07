c66b63171da0d44e5e2c9fb6e0731b04a6634e19
btk1996 <btk_1996@mail.ru>
2026-08-07 09:34:38 +0300

    Update user_config.h

--- app/include/user_config.h
+++ app/include/user_config.h
@@ -88,7 +88,8 @@
 // The example provided executes the LFS module "_init" at startup or fails
 // through to the interactive prompt.
 
-#define LUA_INIT_STRING "pcall(node.flashindex('_init'))"
+#define LUA_INIT_STRING "pcall(function() node.flashindex'_init'() end)"
+//#define LUA_INIT_STRING "pcall(node.flashindex('_init'))"
 
 
 // NodeMCU supports two file systems: SPIFFS and FATFS, the first is available
